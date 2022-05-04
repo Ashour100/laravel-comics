471d7a6af1b0e1c11505f75d51fc81ef52386355
Ashour100 <interinstitute5@gmail.com>
2022-05-04 19:48:49 +0200

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,93 @@
+<div>
+    <div class="FooterTop">
+        <div class="contentContainer flex">
+            <div class="icon flex">
+                <img src="images/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="icon flex">
+                <img src="images/buy-comics-merchandise.png" alt="">
+                <span>DC Merchandise</span>
+            </div>
+            <div class="icon flex">
+                <img src="images/buy-comics-subscriptions.png" alt="">
+                <span>SUBSCRIPTION</span>
+            </div>
+            <div class="icon flex">
+                <img id="locator" src="images/buy-comics-shop-locator.png" alt="">
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+            <div class="icon flex">
+                <img src="images/buy-dc-power-visa.svg" alt="">
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
+    <div class="FooterMiddle flex">
+        <div class="contentContainer flex">
+            <div class="left flex">
+                <div class="list">
+                    <h5>DC COMICS</h5>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <h5>DC</h5>
+                    <ul>
+                        <li>Terms of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certifications</li>
+                        <li>Rating</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <h5>SITES</h5>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>Dc Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+                <div class="list shop-list">
+                    <h5>SHOP</h5>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right">
+                <img src="images/dc-logo-bg.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="FooterBottom">
+        <div class="contentContainer flex">
+            <button>SIGN-UP NOW!</button>
+            <div class="socials flex">
+                <h3>FOLLOW US</h3>
+                <img src="images/footer-facebook.png" alt="">
+                <img src="images/footer-twitter.png" alt="">
+                <img src="images/footer-youtube.png" alt="">
+                <img src="images/footer-pinterest.png" alt="">
+                <img src="images/footer-periscope.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,61 @@
+<div class="header">
+    <div class="contentContainer">
+        <div class="header-logo">
+            <img src="images/dc-logo.png" alt="">
+        </div>
+        <div class="header-nav">
+            <ul>
+                <li>
+                    <a href="">
+                        CHARACTERS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        COMICS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        MOVIES
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        GAMES
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        COLLECTIBLES
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        VIDEOS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        FANS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        NEWS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        SHOP
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
